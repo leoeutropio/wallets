a69a5eb0e066aaa3de02397cd1571ef4e8c2109f
Str0nd4 <leoeutropio@gmail.com>
2017-11-23 19:51:12 -0300

    Atualizações básicas das telas

--- app/src/main/java/com/example/stronda/wallets/LoginGoogleActivity.java
+++ app/src/main/java/com/example/stronda/wallets/LoginGoogleActivity.java
@@ -2,6 +2,9 @@ package com.example.stronda.wallets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.ramotion.foldingcell.FoldingCell;
 
 public class LoginGoogleActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class LoginGoogleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_google);
+        // get our folding cell
     }
+
 }
